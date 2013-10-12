b7189026afdd9c6a0c032f20bcd7b576ead1c11b
mitchthorson <mitchell@mitchellryanthorson.com>
2013-10-12 00:44:26 -0400

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      build: {
+        src: ['js/global/*.js', '!js/global/global.js', 'js/global/global.js'],
+        dest: 'js/global.min.js'
+      }
+    },
+    compass: {                  
+      dist: {                  
+        options: {              
+          sassDir: 'styles/scss',
+          cssDir: 'styles/css',
+          environment: 'production',
+          outputStyle: 'compressed'
+        }
+      }
+    },
+    watch: {
+      styles: {
+        files: ['styles/scss/*.scss'],
+        tasks: ['compass']
+      },
+      scripts: {
+        files: ['js/global/*.js'],
+        tasks: ['uglify']
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['compass', 'uglify']);
+};
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Front-End Starter Template
+
+An HTML 5 Starter template by [Mitchell Thorson](http://mitchellryanthorson.com) for quick-starting simple front-end web projects.
+There are a million good options out there for starter-templates, but I found myself repeating steps to get up and running and decided to make my own starter repo.
+
+##Version 0.1
+There are quite a few things I'd like to add to this as I go, but this is a start.
+
+## Getting Started
+First, you'll need:
+* Ruby
+* Compass ('gem install compass')
+* [Node.js](http://nodejs.org/)
+* Grunt.js ('gem install -g grunt-cli')
+
+Then:
+run 'npm install' from root directory and you're set.
+
+## Compiling SCSS and JS Files
+Run 'grunt' for one-time compiling or 'grunt watch' to watch changes.
+
